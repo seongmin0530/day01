e6d12a58e1e2b2546041c1616bee510ba053121e
kimseongmin <kpmgksm@naver.com>
2023-01-12 17:13:21 +0900

    gui 04

--- day01.py
+++ day01.py
@@ -1,17 +1,8 @@
-#교재 딕셔너리 예제
+import tkinter as tk
 
-subjects = {'의사소통 영어':'A+',
-           '오래된 미래':'B+',
-           '양자역학':'A0'
-           }
-student = '김성민'
-subject = '오래된 미래'
-print(f'ultra modern style: {student}이 수강중인 "{subject}" 수업의 학점은 {subjects[subject]}입니다.')
-# f스트링 = 최신 문법, 문자열과 변수 한번에 출력하고자 할 때 사용한다.
-#ultra modern style
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 Preview')
+win.mainloop()
 
-print("modern style: {0}학생의 {1}과목성적은 {2}입니다.".format(student, subject, subjects[subject]))
-#modern style(format함수)
-
-print('old style: %s 학생의 %s과목 성적은 %s입니다.' % (student, subject, subjects[subject]))
-#old style
+#간단한 그래픽 ui 구성하기 use python
