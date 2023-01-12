b0c48e7589072eacf587a9f9ead4f7282247c79c
kimseongmin <kpmgksm@naver.com>
2023-01-12 17:12:30 +0900

    dictionary 03

--- day01.py
+++ day01.py
@@ -1,14 +1,17 @@
-#교재 countdown 예제
-#for countdown in 5,4,3,2,1,'hey!!':
-#    print(countdown)
+#교재 딕셔너리 예제
 
+subjects = {'의사소통 영어':'A+',
+           '오래된 미래':'B+',
+           '양자역학':'A0'
+           }
+student = '김성민'
+subject = '오래된 미래'
+print(f'ultra modern style: {student}이 수강중인 "{subject}" 수업의 학점은 {subjects[subject]}입니다.')
+# f스트링 = 최신 문법, 문자열과 변수 한번에 출력하고자 할 때 사용한다.
+#ultra modern style
 
-countdown_list = [5,4,3,2,1,'hey!!']
-for countdown in countdown_list:
-    print(countdown)
-    print(countdown_list[2])
-    print(countdown_list[-1])
-#리스트 이용 반복문
+print("modern style: {0}학생의 {1}과목성적은 {2}입니다.".format(student, subject, subjects[subject]))
+#modern style(format함수)
 
-#[] = 리스트
-#{} = 딕셔너리
+print('old style: %s 학생의 %s과목 성적은 %s입니다.' % (student, subject, subjects[subject]))
+#old style
